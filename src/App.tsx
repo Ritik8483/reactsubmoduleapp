2cbe512227c8829de083a21d24c847bf7efda9d1
Ritik Chauhan <ritik.chauhan@quokkalabs.com>
2023-07-18 18:20:26 +0530

    (feat): add date picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { DateRangePicker } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { addDays } from "date-fns";
 
 function App() {
+  const [state, setState] = useState<any>([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: "selection",
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DateRangePicker
+        onChange={(item) => setState([item.selection])}
+        moveRangeOnFirstSelection={false}
+        months={2}
+        ranges={state}
+        direction="horizontal"
+      />
     </div>
   );
 }
